aca80bbeaa995bdaeec70e7a561f1964a61ff571
frreri <49623505+frreri@users.noreply.github.com>
2025-05-28 17:51:43 +0200

    storage handler

--- src/data/storage.js
+++ src/data/storage.js
@@ -1,15 +1,27 @@
-export const emptyForm = {
-  fullName: "",
-  phone: "",
-  age: "",
-  employed: false,
-  salaryOption: "1",
-  amount: "",
-  reason: "",
-  duration: "",
-  message: "",
-};
-const savedFormString = localStorage.getItem("formData");
-export const savedForm = savedFormString
-  ? JSON.parse(savedFormString)
-  : emptyForm;
+class StorageHandler {
+  constructor() {
+    this.emptyForm = {
+      fullName: "",
+      phone: "",
+      age: "",
+      employed: false,
+      salaryOption: "1",
+      amount: "",
+      reason: "",
+      duration: "",
+      message: "",
+    };
+    this.savedForm = this.loadForm();
+  }
+
+  loadForm() {
+    const formString = localStorage.getItem("formData");
+    return formString ? JSON.parse(formString) : this.emptyForm;
+  }
+
+  saveForm(formObj) {
+    localStorage.setItem("formData", JSON.stringify(formObj));
+  }
+}
+
+export default new StorageHandler();
