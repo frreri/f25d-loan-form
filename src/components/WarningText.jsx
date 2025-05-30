27d433f5d229b8f78be9975b2da53f417d3a7550
frreri <49623505+frreri@users.noreply.github.com>
2025-05-30 23:38:11 +0200

    adjusted mobile sizing

--- src/components/WarningText.jsx
+++ src/components/WarningText.jsx
@@ -1,6 +1,6 @@
 function WarningText({ text }) {
   return (
-    <div className="mx-auto mt-2 mb-4 flex w-full max-w-lg items-center justify-center rounded-sm border-2 border-[#d08770] bg-[#ebcb8b] px-4 py-6 md:col-span-2">
+    <div className="mx-auto mt-2 mb-4 flex w-full max-w-[310px] items-center justify-center rounded-sm border-2 border-[#d08770] bg-[#ebcb8b] px-4 py-6 md:col-span-2 md:max-w-lg">
       <p>{text}</p>
     </div>
   );
